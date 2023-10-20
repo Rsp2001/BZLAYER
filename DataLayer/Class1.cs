1be9da5380f63d119f87f3b63c1b0b765b43a566
Rsp2001 <montypatil51976@gmail.com>
2023-10-20 11:30:18 +0000

    Demo

---
+++ DataLayer/Class1.cs
@@ -0,0 +1,19 @@
+﻿using Entity;
+
+namespace DataLayer;
+public class DataAccess
+{
+    public static List<Movies> movies = new List<Movies>()
+    {
+        new Movies{Id=1, Name="Toofan",Ryear=2021,Rating=3},
+        new Movies{Id=2, Name="Ludo",Ryear=2020,Rating=4},
+        new Movies{Id=3, Name="Gunjan",Ryear=2020,Rating=1},
+        new Movies{Id=4, Name="Big Bull",Ryear=2022,Rating=3},
+        new Movies{Id=5, Name="Laxmi",Ryear=2020,Rating=2}
+    };
+
+    public  List<Movies> GetMovies()
+    {
+        return movies;
+    }
+}
